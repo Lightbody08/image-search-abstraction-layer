8e211c0102358075a64ec2f86d8071f699c5aa0a
Ryan Lightbody <Ryan Lightbody>
2016-07-28 16:04:37 -0400

    Initial Commit

---
+++ server.js
@@ -0,0 +1,14 @@
+var app = require('express')();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(process.env.PORT || 80, function () {
+	if (process.env.PORT) {
+		console.log('Example app listening on port' + process.env.PORT + '!');
+	}
+	if (80) {
+		console.log('Example app listening on port 80!');
+	}
+});
