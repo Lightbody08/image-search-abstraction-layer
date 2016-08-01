673f69142ff47815925d02bac9524f91a6dbf8a3
Ryan Lightbody <Ryan Lightbody>
2016-08-01 17:42:04 -0400

    added Bing search API

--- server.js
+++ server.js
@@ -1,14 +1,39 @@
 var app = require('express')();
+var mongodb = require('mongodb');
+var MongoClient = mongodb.MongoClient;
+var mongoURL = process.env.MONGOLAB_URI;
+var bingAPI = process.env.BING_API_KEY;
+var Search = require('bing.search');
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+var search = new Search(bingAPI);
+
+app.get('/imagesearch/:image', searchImages);
+
+function searchImages (req, res) {
+	var requestedImg = req.params.image;
+	search.images(requestedImg, {
+  		top: 10
+  	},
+  	function (err, results) {
+  		if (err) throw err;
+  		res.send(results.map(createOutput));
+  	}
+  );
+}
+
+function createOutput (image) {
+	return {
+		"url": image.url,
+		"snippet": image.title,
+		"thumbnail": image.thumbnail.url,
+		"context": image.sourceUrl
+	};
+}
 
 app.listen(process.env.PORT || 80, function () {
 	if (process.env.PORT) {
 		console.log('Example app listening on port' + process.env.PORT + '!');
-	}
-	if (80) {
+	} else {
 		console.log('Example app listening on port 80!');
 	}
 });
